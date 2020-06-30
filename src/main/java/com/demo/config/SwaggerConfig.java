7a48aec468b136d6ad76e51cb126d390a7dfde6c
dyx77421088 <2639074625@qq.com>
2020-06-30 16:55:47 +0800

    swagger

--- src/main/java/com/demo/config/SwaggerConfig.java
+++ src/main/java/com/demo/config/SwaggerConfig.java
@@ -1,4 +1,45 @@
 package com.demo.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
+
+    @Bean
+    public Docket dyxDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .enable(true)
+                .groupName("杜煜兴")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.demo.controller"))
+                .paths(PathSelectors.ant("/user/**"))
+                .build()
+                ;
+    }
+
+    private ApiInfo apiInfo() {
+        Contact contact =  new Contact("杜煜兴", "", "");
+
+        return new ApiInfo(
+                "我是标题",
+                "我是描述信息",
+                "6.6",
+                "urn:tos",
+                contact,
+                "Apache 2.0",
+                "http://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList());
+    }
 }
