4bcfcecbbe8852c68a640daaf84fe0541c2231c7
dyx77421088 <2639074625@qq.com>
2020-06-30 17:12:22 +0800

    user

--- src/main/java/com/demo/controller/UserController.java
+++ src/main/java/com/demo/controller/UserController.java
@@ -1,4 +1,17 @@
 package com.demo.controller;
 
+import com.demo.pojo.User;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @PostMapping("/login")
+    public User login(@ApiParam("用户信息") User user) {
+        return user;
+    }
 }
--- src/main/java/com/demo/pojo/User.java
+++ src/main/java/com/demo/pojo/User.java
@@ -1,4 +1,29 @@
 package com.demo.pojo;
 
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel("用户")
 public class User {
+    @ApiModelProperty("用户名")
+    private String userName;
+    @ApiModelProperty("密码")
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
